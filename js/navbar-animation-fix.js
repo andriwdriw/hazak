fc51a581b3b034b7c7f3416e6bd3c4bcaa3373a9
[andriw-admin] <andriw.devetrix@gmail.com>
2018-11-11 19:12:45 -0200

    projeto inicial

---
+++ js/navbar-animation-fix.js
@@ -0,0 +1,15 @@
+$('#menu-collapse').on('show.bs.collapse', function () {
+	$('#carrossel-geral').css('transform', 'translate(-10%, 50%)')
+});
+$('#menu-collapse').on('hide.bs.collapse', function () {
+	$('#carrossel-geral').css('transform', 'translate(0%, 0%)')
+});
+
+
+
+
+
+
+$('#botao-produto').on('show.bs.collapse', function() {
+	$('.caixa-produto').css('transform')
+});
